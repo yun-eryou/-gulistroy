108529a19a7643a925a190aee573f156b15f36e9
Yuneryou <166462894@qq.com>
2022-09-20 22:51:00 +0800

    在BrandEntry类中，以及BrandController中添加了分组校验功能，也添加了自定义校验功能。

--- guilimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
+++ guilimall-product/src/main/java/com/atguigu/gulimall/product/controller/BrandController.java
@@ -1,10 +1,16 @@
 package com.atguigu.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 
+import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.UpdateGroup;
+import com.atguigu.common.valid.UpdateStatusGroup;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +22,7 @@ import com.atguigu.gulimall.product.service.BrandService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
 
+import javax.validation.Valid;
 
 
 /**
@@ -36,7 +43,7 @@ public class BrandController {
      */
     @RequestMapping("/list")
     //@RequiresPermissions("product:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -48,8 +55,8 @@ public class BrandController {
      */
     @RequestMapping("/info/{brandId}")
     //@RequiresPermissions("product:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -59,9 +66,24 @@ public class BrandController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@Validated({AddGroup.class}) @RequestBody BrandEntity brand) {
+        //    所有的异常都被统一处理了，所以不需要单独处理这个异常了
+        //if (result.hasErrors()) {
+        //    Map<String, String> map = new HashMap<>();
+        //    // 1,获取校验的错误结果
+        //    result.getFieldErrors().forEach((item) -> {
+        //        // 获取到错误提示
+        //        String message = item.getDefaultMessage();
+        //        // 获取错误的属性名字
+        //        String field = item.getField();
+        //        map.put(field,message);
+        //    });
+        //   return R.error(400, "提交的数据不合法").put("data",map);
+        //}else {
+        //
+        //}
 
+        brandService.save(brand);
         return R.ok();
     }
 
@@ -70,8 +92,21 @@ public class BrandController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
+    public R update(@Validated(UpdateGroup.class)@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改状态
+     * @param brand
+     * @return
+     */
+    @RequestMapping("/update/status")
+    //@RequiresPermissions("product:brand:update")
+    public R updateStatus(@Validated(UpdateStatusGroup.class)@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
 
         return R.ok();
     }
@@ -81,8 +116,8 @@ public class BrandController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("product:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }
---
+++ gulimall-commo/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
@@ -0,0 +1,28 @@
+package com.atguigu.common.exception;
+
+public enum BizCodeEnume {
+
+    UNKNOW_EXCEPTION(10000,"系统位置异常"),
+    VALID_EXCEPTION(10001,"参数格式校验失败");
+
+    private Integer code;
+    private String msg;
+
+    BizCodeEnume(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+
+
+    public Integer getCode() {
+        return code;
+    }
+
+
+}
